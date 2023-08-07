b719b19ba82215efa86d3100910b3f2be3a4e73e
dokaioka <dewaoka02@gmail.com>
2023-08-07 19:18:45 +0800

    add sesi 6

---
+++ sesi-05/INPG-PKJ-10-015_I DEWA GEDE ARI OKA_Tugas5/Latihan/Events Click/script.js
@@ -0,0 +1,8 @@
+function ubahText() {
+  var element = document.getElementById('tombol');
+  if (element.innerHTML == 'Klik aku') {
+    element.innerHTML = 'Halo YAL';
+  } else {
+    element.innerHTML = 'Klik aku';
+  }
+}
